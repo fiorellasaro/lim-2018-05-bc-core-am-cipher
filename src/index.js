4dabadb74f5784450f9e1f7ea653bbc14517c2cc
fiorellasaro <fiorella.sfl@gmail.com>
2018-05-23 06:51:33 -0500

    Primera implementacion de la logica para el cifrado. Aun sin DOM

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+let cifrado = (texto, nOffSet) => {
+
+let letterInicial = ''; //Variable tipo string que almacenara el texto codificado
+let letterAscii; //variale que almacenara la letra que provenia de su numero ascii
+let nOffSet = 5;  //Variable que contendrá el Offset, el valor de N, el cual indica el usuario
+let numberAscii; //variable que contendrá el numero ascii que le corresponde a cada caracter del texto a codificar
+let indice = 0; // variable que indica el indice de un caracter dentro del texto a codificar
+let charCounter = 0; // variable que contará el número de caracteres en el texto a codificar. Su finalidad es que cuando que cuando esta variable iguale el numero de caracteres del texto, el bucle se romperá
+let texto = 'hola'; 
+while (texto.length !== charCounter){
+numberAscii = texto.charCodeAt(indice);
+letterAscii = String.fromCharCode(numberAscii+nOffSet);
+letterInicial = letterInicial.concat(letterAscii);
+charCounter ++;
+indice ++;
+}
+
+alert(letterInicial);
+}
+
+
+
