67d0445f0209fc486900790803d7dfefa61afccd
fiorellasaro <fiorella.sfl@gmail.com>
2018-05-23 12:58:25 -0500

    Metodos cifrado y decifrado implementados, comienzos de DOM

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
+let texto = document.getElementsByName('CipherBoxText')[0];
+alert(texto); 
+let nOffSet1 = document.getElementsByClassName('nValue')[0]; //Variable que contendrá el Offset, el valor de N, el cual indica el usuario
 
-let cifrado = (texto, nOffSet) => {
+parseInt(nOffSet1);
 
-let letterInicial = ''; //Variable tipo string que almacenara el texto codificado
-let letterAscii; //variale que almacenara la letra que provenia de su numero ascii
-let nOffSet = 5;  //Variable que contendrá el Offset, el valor de N, el cual indica el usuario
-let numberAscii; //variable que contendrá el numero ascii que le corresponde a cada caracter del texto a codificar
-let indice = 0; // variable que indica el indice de un caracter dentro del texto a codificar
-let charCounter = 0; // variable que contará el número de caracteres en el texto a codificar. Su finalidad es que cuando que cuando esta variable iguale el numero de caracteres del texto, el bucle se romperá
-let texto = 'hola'; 
-while (texto.length !== charCounter){
-numberAscii = texto.charCodeAt(indice);
-letterAscii = String.fromCharCode(numberAscii+nOffSet);
-letterInicial = letterInicial.concat(letterAscii);
-charCounter ++;
-indice ++;
-}
+document.getElementsByClassName('buttonCipher').addEventListener('click', cipher.encode(texto,nOffSet1));
+document.getElementsByClassName('buttonDecipher').addEventListener('click', cipher.decode(texto,nOffSet1));
 
-alert(letterInicial);
-}
+
+
+
 
 
 
