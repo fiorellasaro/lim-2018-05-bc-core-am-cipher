868ce852e56c5159cd10536f9f41e47567436f97
fiorellasaro <fiorella.sfl@gmail.com>
2018-05-25 06:04:55 -0500

    Implementacion de cifrado y descifrado completada

--- src/cipher.js
+++ src/cipher.js
@@ -8,7 +8,16 @@ window.cipher = {
     let charCounter = 0; // variable que contará el número de caracteres en el texto a codificar. Su finalidad es que cuando que cuando esta variable iguale el numero de caracteres del texto, el bucle se romperá
     while ( text.length !== charCounter){
       numberAscii = text.charCodeAt(indice);
-      letterAscii = String.fromCharCode(numberAscii+nOffSet);
+    if(numberAscii>=65 && numberAscii<=90){
+        letterAscii = String.fromCharCode((numberAscii - 65 + nOffSet)%26 + 65);
+        }else {
+          if(numberAscii>=97 && numberAscii<=122){
+            letterAscii = String.fromCharCode((numberAscii - 97 + nOffSet)%26 + 97);
+          }else{
+            letterAscii = String.fromCharCode(numberAscii+nOffSet);//else final, despues de evaluar si son del abecedario (mayusculas o minusculas)
+          }
+        }
+
       letterInicial = letterInicial.concat(letterAscii);
       charCounter ++;
       indice ++;
@@ -24,11 +33,28 @@ window.cipher = {
     let charCounter = 0; // variable que contará el número de caracteres en el texto a codificar. Su finalidad es que cuando que cuando esta variable iguale el numero de caracteres del texto, el bucle se romperá
     while ( text.length !== charCounter){
       numberAscii = text.charCodeAt(indice);
-      letterAscii = String.fromCharCode(numberAscii-nOffSet);
+      if(numberAscii>=65 && numberAscii<=90){
+        if(numberAscii - 65 - nOffSet>=0){
+          letterAscii = String.fromCharCode((numberAscii - 65 - nOffSet)%26 +65);
+        }else{
+          letterAscii = String.fromCharCode((numberAscii - 65 - nOffSet)%26 +91);
+        }
+      }else {
+          if(numberAscii>=97 && numberAscii<=122){
+            if(numberAscii - 97 - nOffSet>=0){
+              letterAscii = String.fromCharCode((numberAscii - 97 - nOffSet)%26 +97);
+            }else{
+              letterAscii = String.fromCharCode((numberAscii - 97 - nOffSet)%26 +123);
+            } 
+          }else{
+            letterAscii = String.fromCharCode(numberAscii-nOffSet);//else final, despues de evaluar si son del abecedario (mayusculas o minusculas)
+          }
+        }
+
       letterInicial = letterInicial.concat(letterAscii);
       charCounter ++;
       indice ++;
-      }
+    }
     document.getElementById("TextoAMostrar").value = letterInicial;
-      } 
+  }
 };
