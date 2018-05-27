8fcaaa2fd70bb6b8debdb7d4e3993f08b333320e
fiorellasaro <fiorella.sfl@gmail.com>
2018-05-27 09:49:08 -0500

    metodos cifrado y decifrado implementados al 100% con nueva formula

--- src/cipher.js
+++ src/cipher.js
@@ -6,17 +6,26 @@ window.cipher = {
     let numberAscii; //variable que contendrá el numero ascii que le corresponde a cada caracter del texto a codificar
     let indice = 0; // variable que indica el indice de un caracter dentro del texto a codificar
     let charCounter = 0; // variable que contará el número de caracteres en el texto a codificar. Su finalidad es que cuando que cuando esta variable iguale el numero de caracteres del texto, el bucle se romperá
+    nOffSet = nOffSet%26;
     while ( text.length !== charCounter){
       numberAscii = text.charCodeAt(indice);
-    if(numberAscii>=65 && numberAscii<=90){
-        letterAscii = String.fromCharCode((numberAscii - 65 + nOffSet)%26 + 65);
-        }else {
-          if(numberAscii>=97 && numberAscii<=122){
-            letterAscii = String.fromCharCode((numberAscii - 97 + nOffSet)%26 + 97);
+      if(numberAscii>=65 && numberAscii<=90){
+        if(numberAscii+nOffSet>90){
+          letterAscii = String.fromCharCode(numberAscii + nOffSet - 26);
           }else{
-            letterAscii = text.charAt(indice);
+          letterAscii = String.fromCharCode(numberAscii + nOffSet);
           }
-        }
+      } else {
+          if(numberAscii>=97 && numberAscii<=122){
+             if(numberAscii+nOffSet>122){
+              letterAscii = String.fromCharCode(numberAscii + nOffSet - 26);
+              }else{
+              letterAscii = String.fromCharCode(numberAscii + nOffSet);
+              }
+          }else {
+            letterAscii = text.charAt(indice);
+            }
+      }
 
       letterInicial = letterInicial.concat(letterAscii);
       charCounter ++;
@@ -31,25 +40,26 @@ window.cipher = {
     let numberAscii; 
     let indice = 0; 
     let charCounter = 0; 
+    nOffSet = nOffSet%26;
     while ( text.length !== charCounter){
       numberAscii = text.charCodeAt(indice);
       if(numberAscii>=65 && numberAscii<=90){
-        if((numberAscii - 65 - nOffSet)>=0 (numberAscii - 65 - nOffSet)== -nOffSet){
-          letterAscii = String.fromCharCode((numberAscii - 65 - nOffSet)%26 +65);
-        }else{
-          letterAscii = String.fromCharCode((numberAscii - 65 - nOffSet)%26 +91);
-        }
-      }else {
-          if(numberAscii>=97 && numberAscii<=122){
-            if((numberAscii - 97 - nOffSet)>=0 || (numberAscii - 97 - nOffSet) == -nOffSet ){
-              letterAscii = String.fromCharCode((numberAscii - 97 - nOffSet)%26 +97);
-            }else{
-              letterAscii = String.fromCharCode((numberAscii - 97 - nOffSet)%26 +123);
-            } 
+        if(numberAscii-nOffSet<65){
+          letterAscii = String.fromCharCode(numberAscii - nOffSet + 26);
           }else{
-            letterAscii = text.charAt(indice);
+          letterAscii = String.fromCharCode(numberAscii - nOffSet);
           }
-        }
+      } else {
+          if(numberAscii>=97 && numberAscii<=122){
+             if(numberAscii-nOffSet<97){
+              letterAscii = String.fromCharCode(numberAscii - nOffSet + 26);
+              }else{
+              letterAscii = String.fromCharCode(numberAscii - nOffSet);
+              }
+          }else {
+            letterAscii = text.charAt(indice);
+            }
+      }
 
       letterInicial = letterInicial.concat(letterAscii);
       charCounter ++;
