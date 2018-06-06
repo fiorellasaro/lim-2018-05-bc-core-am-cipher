8e67e4bb66653aea66d70cefbef011550186564a
fiorellasaro <fiorella.sfl@gmail.com>
2018-06-06 00:41:43 -0500

    Semantica HTML completa, CSS completo para moviles, DOM completo. Falta responsive para Ipad y desktop.

--- src/index.js
+++ src/index.js
@@ -4,44 +4,61 @@ let nOffSetUser;
 let numberN;
 
 let capturarDatos = () => {
-    texto = document.getElementById('TextToCipherOrDecipher').value;
-    nOffSetUser = document.getElementById('nValue').value; //Variable que contendrá el Offset, el valor de N, el cual indica el usuario    
+    texto = document.getElementsByName('CipherBoxText')[0].value;
+    nOffSetUser = document.getElementsByName('OffSet')[0].value; //Variable que contendrá el Offset, el valor de N, el cual indica el usuario    
     numberN = parseInt(nOffSetUser);
 } 
 
 let ShowResultsEncode = () => {   
     capturarDatos();
-    document.getElementById('TextoAMostrar').innerText = cipher.encode(numberN, texto);
+    document.getElementsByClassName('ConvertAndResults')[1].getElementsByTagName('p')[0].innerText = cipher.encode(numberN, texto);
 }
 
 
 let ShowResultsDecode = () => {   
     capturarDatos ();
-    document.getElementById('TextoAMostrar').innerText = cipher.decode(numberN, texto);
+    document.getElementsByClassName('ConvertAndResults')[1].getElementsByTagName('p')[0].innerText = cipher.decode(numberN, texto);
 }
 
+
 let ShowInstructionsCipher = () => {
-    document.getElementById('InstructionsCipher').style.visibility = 'hidden';
-    document.getElementById('InstructionsCipher').style.display = 'none';
-    document.getElementById('InstructionsCipherText').style.marginLeft = '25%';
-    document.getElementById('InstructionsCipherText').style.marginRight = '15px';
-    document.getElementById('InstructionsCipherText').style.visibility = 'visible';
+    document.getElementsByClassName('InstructionsButton')[0].style.visibility = 'hidden';
+    document.getElementsByClassName('InstructionsButton')[0].style.display = 'none';
+    document.getElementsByClassName('InstructionsText')[0].style.display = 'inherit';
+    document.getElementsByClassName('InstructionsText')[0].style.visibility = 'visible';
    
 } 
 
 let ShowInstructionsDecipher = () => {
-    document.getElementById('InstructionsDecipher').style.visibility = 'hidden';
-    document.getElementById('InstructionsDecipher').style.display = 'none';
-    document.getElementById('InstructionsDecipherText').style.visibility = 'visible';
+    document.getElementsByClassName('InstructionsButton')[1].style.visibility = 'hidden';
+    document.getElementsByClassName('InstructionsButton')[1].style.display = 'none';
+    document.getElementsByClassName('InstructionsText')[1].style.display = 'inherit';
+    document.getElementsByClassName('InstructionsText')[1].style.visibility = 'visible';
 } 
 
+let hiddenInstructionsCipher = () => {
+    document.getElementsByClassName('InstructionsText')[0].style.visibility = 'hidden';
+    document.getElementsByClassName('InstructionsText')[0].style.display = 'none';
+    document.getElementsByClassName('InstructionsButton')[0].style.display = 'inherit';
+    document.getElementsByClassName('InstructionsButton')[0].style.visibility = 'visible';
+}
+
+let hiddenInstructionsDecipher = () => {
+    document.getElementsByClassName('InstructionsText')[1].style.visibility = 'hidden';
+    document.getElementsByClassName('InstructionsText')[1].style.display = 'none';
+    document.getElementsByClassName('InstructionsButton')[1].style.display = 'inherit';
+    document.getElementsByClassName('InstructionsButton')[1].style.visibility = 'visible';
+}
 
    
 
-document.getElementById('buttonCipher').addEventListener('click', ShowResultsEncode);
-document.getElementById('buttonDecipher').addEventListener('click', ShowResultsDecode);
-document.getElementById('InstructionsCipher').addEventListener('click', ShowInstructionsCipher);
-document.getElementById('InstructionsDecipher').addEventListener('click', ShowInstructionsDecipher);
+document.getElementsByClassName('buttonsAndOffset')[0].getElementsByTagName('button')[0].addEventListener('click', ShowResultsEncode);
+document.getElementsByClassName('buttonsAndOffset')[0].getElementsByTagName('button')[1].addEventListener('click', ShowResultsDecode);
+
+document.getElementsByClassName('InstructionsButton')[0].addEventListener('click', ShowInstructionsCipher);
+document.getElementsByClassName('InstructionsButton')[1].addEventListener('click', ShowInstructionsDecipher);
+document.getElementsByClassName('InstructionsText')[0].addEventListener('click', hiddenInstructionsCipher);
+document.getElementsByClassName('InstructionsText')[1].addEventListener('click', hiddenInstructionsDecipher);
 
 
 
